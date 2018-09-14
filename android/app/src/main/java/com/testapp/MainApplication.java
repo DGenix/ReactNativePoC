5a52e6cbb3a3a38c7cce1cce8f0fc27b8d90cf45
FabriBertani <fabri_bertani@hotmail.com>
2018-09-14 18:09:40 -0300

    Added react-native-maps library and MapView to FindUsScreen.

--- android/app/src/main/java/com/testapp/MainApplication.java
+++ android/app/src/main/java/com/testapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.testapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new FastImageViewPackage(),
             new RNFetchBlobPackage(),
           new VectorIconsPackage(),
--- app/screens/FindUsScreen.js
+++ app/screens/FindUsScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MapView, { Marker } from 'react-native-maps';
 
 export default class FindUsScreen extends React.Component {
     static navigationOptions = {
@@ -13,7 +14,43 @@ export default class FindUsScreen extends React.Component {
     render() {
         return (
             <View style={styles.content}>
-
+                <MapView
+                    style={styles.map}
+                    provider={undefined}
+                    initialRegion={{
+                        latitude: -32.945813,
+                        longitude: -60.641726,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121
+                    }}
+                    showsScale={true}
+                    rotateEnabled={true}
+                    cacheEnabled={true}
+                    loadingEnabled={true}
+                    zoomEnabled={true}
+                    zoomControlEnabled={true}
+                    minZoomLevel={17} >
+                    <Marker
+                        coordinate={{
+                            latitude: -32.945813,
+                            longitude: -60.641726
+                        }}
+                        title="D-Genix"
+                        description="Córdoba 1365, 7 1"
+                    />
+                </MapView>
+                <View style={styles.row}>
+                    <FontAwesome name="map-marker" size={35} color="red" />
+                    <Text style={styles.textStyle}>
+                        Córdoba 1365, 7th Floor, Office 1
+                    </Text>
+                </View>
+                <Text style={{...styles.textStyle, marginLeft: 50}}>
+                    Rosario, Santa Fe
+                </Text>
+                <Text style={{...styles.textStyle, marginLeft: 50}}>
+                    Argentina
+                </Text>
             </View>
         )
     }
@@ -24,5 +61,22 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         backgroundColor: 'white'
-    }
+    },
+    map: {
+        width: '100%',
+        height: '70%'
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        marginLeft: 15,
+        marginTop: 10
+    },
+    textStyle: {
+        fontSize: 20,
+        color: 'black',
+        marginLeft: 10,
+        fontWeight: 'bold'
+    }    
 })
